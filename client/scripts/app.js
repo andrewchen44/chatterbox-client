6568f7bff34d6d566f3377191c08bbf6251e44f3
andrewchen44 <andrewchen.c44@gmail.com>
2017-10-20 15:03:36 -0700

    Updated config file as well as created post and get ajax requests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,37 @@
 // YOUR CODE HERE:
+var message = {
+  username: 'Dandrew',
+  text: 'Hello Everyone',
+  roomname: '4chan'
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Messages Recieved', data);
+    var $chat = $('<div>"hi"</div>');
+    $('#chats').prepend($chat);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
