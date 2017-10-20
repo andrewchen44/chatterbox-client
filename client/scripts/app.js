3cdb2c4477be551091c1e025f47a13f163685d64
andrewchen44 <andrewchen.c44@gmail.com>
2017-10-20 15:31:07 -0700

    Made page display all messages and added some style

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,12 @@ var message = {
   roomname: '4chan'
 };
 
+    
+    //container div
+    //head div, append it to the container
+    //make a message div, append head
+    //make a time div, append to message div
+
 $.ajax({
   // This is the url you should use to communicate with the parse API server.
   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
@@ -27,11 +33,30 @@ $.ajax({
   contentType: 'application/json',
   success: function (data) {
     console.log('chatterbox: Messages Recieved', data);
-    var $chat = $('<div>"hi"</div>');
-    $('#chats').prepend($chat);
+    //loop to go through all our data
+    for (var i = 0; i < data.results.length; i++) {
+      var $container = $('<div class="container"></div>');
+      var $message = $('<div class="message"></div>');
+      var $name = $('<div class="name"></div>');
+      var $time = $('<div class="time"></div>');
+      
+      $message.text(data.results[i].text);
+      $name.text(data.results[i].username);
+      $time.text(data.results[i].createdAt);
+      
+      //insert the message text into our container div
+      $('#chats').prepend($container);
+      $container.append($name);
+      $container.append($message);
+      $container.append($time);
+    }
+      //append container dive to the chats class
+    
   },
   error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
     console.error('chatterbox: Failed to send message', data);
   }
 });
+
+
