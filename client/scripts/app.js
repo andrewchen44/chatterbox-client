68024c8e52ddb411752257254255edafc79c608d
andrewchen44 <andrewchen.c44@gmail.com>
2017-10-20 19:45:31 -0700

    adding friend functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+  var roomNames = {};
+  var friends = {};
   
   var outputMessages = function(data) {
     for (var i = 0; i < data.results.length; i++) {
-      var $container = $('<div class="container"></div>');
+      var $container = $('<div class="chat"></div>');
       var $message = $('<div class="message"></div>');
-      var $name = $('<div class="name"></div>');
+      var $name = $('<div class="username"></div>');
       var $time = $('<div class="time"></div>');
       
       $message.text(data.results[i].text);
@@ -19,40 +21,16 @@ $(document).ready(function() {
     }
   };
   
-  
-  
-  
-  $('.roomNames').change(function() {
-    var room = $(this).val();
-    var url = `http://parse.sfm8.hackreactor.com/chatterbox/classes/messages/?where={"roomname":"${room}"}`;
-    console.log(url);
-    $.ajax({
-      url: url,
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
-        $('#chats').html('');
-        outputMessages(data);
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to get new data', data);
-      }
-    });
-  });
-  
-
-
   $.ajax({
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
       // iterate over all objects in array
-      var lookup = {};
       for (var i = 0; i < data.results.length; i++) {
         var room = data.results[i].roomname;
-        if (!lookup[room] && room) {
-          lookup[room] = true;
+        if (!roomNames[room] && room) {
+          roomNames[room] = true;
           var $newRoom = $(`<option value="${room}">${room}</option>`);
           $('select').append($newRoom);
         }
@@ -63,8 +41,90 @@ $(document).ready(function() {
       console.error('chatterbox: Failed to send message', data);
     }
   });
-
+  
+  
+  //filters messages by room when changed in drop down
+  $('.roomNames').change(function() {
+    console.log('changed');
+    var room = $(this).val();
+    var url = `http://parse.sfm8.hackreactor.com/chatterbox/classes/messages/?where={"roomname":"${room}"}`;
+    $.ajax({
+      url: url,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        $('#chats').html('');
+        outputMessages(data);
+      },
+      error: function (data) {
+        console.error('error');
+      }
+    });
+  });
+  
+  //submits the message to the server
+  $('input[type="submit"]').on('click', function(event) {
+    event.preventDefault();
+    var text = $('input[type="text"]').val();
+    $('input[type="text"]').val('');
+    var room = $('.roomNames').val();
+    var message = {
+      username: 'Dandrew',
+      text: text,
+      roomname: room
+    };
+    
+    $.ajax({
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        var $container = $('<div class="container"></div>');
+        var $message = $('<div class="message"></div>');
+        var $name = $('<div class="name"></div>');
+        var $time = $('<div class="time"></div>');
+      
+        $message.text(text);
+        $name.text('Dandrew');
+        $time.text(new Date().toLocaleString());
+      
+      //insert the message text into our container div
+        $('#chats').prepend($container);
+        $container.append($name);
+        $container.append($message);
+        $container.append($time);
+        
+      },
+      error: function (data) {
+        console.error('chatterbox: failed to send new message', data);
+      }
+    });  
+  });
+  
+  //adds a new room
+  $('.addroom').on('click', function(event) {
+    event.preventDefault();
+    var newRoom = prompt('Enter new room name.');
+    if (newRoom && !roomNames[newRoom]) {
+      var $newRoom = $(`<option value="${newRoom}" selected>${newRoom}</option>`);
+      $('select').prepend($newRoom);
+      $('#chats').html('');
+    }
+  }); 
+  
+  $('div.username').on('click', function(event) {
+    console.log('asdf');
+    event.preventDefault();
+    var name = $(this).val();
+  }); 
+  
 });
+  
+  //sort by time
+  //moment.js library
+
+
 
 
 
